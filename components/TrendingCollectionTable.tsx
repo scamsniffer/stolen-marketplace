e1606f51eb310e1a6b0277f743518f4ee0223832
fun <lljxx1@gmail.com>
2022-06-29 11:01:02 +0800

    order by value

--- components/TrendingCollectionTable.tsx
+++ components/TrendingCollectionTable.tsx
@@ -45,69 +45,76 @@ const TrendingCollectionTable: FC<Props> = ({ fallback }) => {
           </tr>
         </thead>
         <tbody>
-          {mappedCollections?.map((collection: any, index, arr) => {
-            const {
-              contract,
-              tokenHref,
-              image,
-              name,
-              valueInETH,
-              stolen,
-              days1,
-              days30,
-              days7,
-              days1Change,
-              days7Change,
-              days30Change,
-              floorSaleChange1Days,
-              floorSaleChange7Days,
-              floorSaleChange30Days,
-              floorPrice,
-              supply,
-            } = processCollection(collection)
+          {mappedCollections
+            ?.map((collection: any, index, arr) => {
+              return processCollection(collection)
+            })
+            .sort((a, b) => {
+              return b.valueInETH - a.valueInETH
+            })
+            .map((parsed: any, index, arr) => {
+              const {
+                contract,
+                tokenHref,
+                image,
+                name,
+                valueInETH,
+                stolen,
+                days1,
+                days30,
+                days7,
+                days1Change,
+                days7Change,
+                days30Change,
+                floorSaleChange1Days,
+                floorSaleChange7Days,
+                floorSaleChange30Days,
+                floorPrice,
+                supply,
+              } = parsed
 
-            return (
-              <tr
-                key={`${contract}-${index}`}
-                // ref={index === arr.length - 5 ? ref : null}
-                className="group h-[88px] border-b border-neutral-300 dark:border-neutral-600 dark:text-white"
-              >
-                {/* COLLECTION */}
-                <td className="reservoir-body flex items-center gap-4 whitespace-nowrap px-6 py-4 dark:text-white">
-                  <div className="reservoir-h6 mr-6 dark:text-white">
-                    {index + 1}
-                  </div>
-                  <Link href={tokenHref}>
-                    <a className="flex items-center gap-2">
-                      <img
-                        src={optimizeImage(image, 140)}
-                        className="h-[56px] w-[56px] rounded-full object-cover"
-                      />
-                      <div
-                        className={`reservoir-h6 overflow-hidden truncate whitespace-nowrap dark:text-white ${
-                          isSmallDevice ? 'max-w-[140px]' : ''
-                        }`}
-                      >
-                        {name}
-                      </div>
-                    </a>
-                  </Link>
-                </td>
-
-                {/* FLOOR PRICE */}
-                <td className="reservoir-body whitespace-nowrap px-6 py-4 dark:text-white">
-                  <FormatEth amount={valueInETH} />
-                </td>
-
-                {/* SUPPLY */}
-                {
-                  <td className="reservoir-body whitespace-nowrap px-6 py-4 dark:text-white">
-                    {stolen ? formatNumber(+stolen) : '-'}
+              return (
+                <tr
+                  key={`${contract}-${index}`}
+                  // ref={index === arr.length - 5 ? ref : null}
+                  className="group h-[88px] border-b border-neutral-300 dark:border-neutral-600 dark:text-white"
+                >
+                  {/* COLLECTION */}
+                  <td className="reservoir-body flex items-center gap-4 whitespace-nowrap px-6 py-4 dark:text-white">
+                    <div className="reservoir-h6 mr-6 dark:text-white">
+                      {index + 1}
+                    </div>
+                    <Link href={tokenHref}>
+                      <a className="flex items-center gap-2">
+                        <img
+                          src={optimizeImage(image, 140)}
+                          className="h-[56px] w-[56px] rounded-full object-cover"
+                        />
+                        <div
+                          className={`reservoir-h6 overflow-hidden truncate whitespace-nowrap dark:text-white ${
+                            isSmallDevice ? 'max-w-[140px]' : ''
+                          }`}
+                        >
+                          {name}
+                        </div>
+                      </a>
+                    </Link>
                   </td>
-                }
-              </tr>
-            )
-          })}
+
+                  {/* FLOOR PRICE */}
+                  <td className="reservoir-body whitespace-nowrap px-6 py-4 dark:text-white">
+                    <FormatEth amount={valueInETH} />
+                  </td>
+
+                  {/* SUPPLY */}
+                  {
+                    <td className="reservoir-body whitespace-nowrap px-6 py-4 dark:text-white">
+                      {stolen ? formatNumber(+stolen) : '-'}
+                    </td>
+                  }
+                </tr>
+              )
+            })}
         </tbody>
       </table>
     </div>
